00d987fcb769b032d93fc60c81a87d9aeca328fd
ahasanUllah <88593825+ahasanUllah@users.noreply.github.com>
2022-09-15 09:20:56 +0600

    math round change to parse int for better result

--- script.js
+++ script.js
@@ -68,6 +68,8 @@ const gameOver = () => {
    // the current time is the finish time
    // so total time taken is current time - start time
    const finishTime = new Date().getTime();
+   console.log(finishTime, startTime);
+
    const timeTaken = (finishTime - startTime) / 1000;
 
    // show result modal
@@ -81,7 +83,7 @@ const gameOver = () => {
    // show result
    resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${parseInt(timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -135,5 +137,7 @@ displayHistory();
 setInterval(() => {
    const currentTime = new Date().getTime();
    const timeSpent = (currentTime - startTime) / 1000;
-   document.getElementById('show-time').innerHTML = `${startTime ? Math.round(timeSpent) : 0} seconds`;
+   console.log(timeSpent);
+
+   document.getElementById('show-time').innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
